90538e84dafa44b886b59d9257e47d8eac52f34d
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-03 10:59:28 +0100

    finished basic

---
+++ script.js
@@ -0,0 +1,34 @@
+// var text="Hi my name is Martin";
+// console.log(text);
+// document.write(text);
+
+
+// ---------------------------
+
+// function myFunction() {
+//     var str1 = "and I am 25 years old";
+//     var str2 = "Hi my name is Martin";
+
+//     // var n = str.indexOf("welcome");
+//     document.getElementById("new_text1").innerHTML =str1;
+//     document.getElementById("new_text2").innerHTML =str2;
+
+    
+
+//   }
+
+console.log(players[2]);
+
+
+function myFunction() {
+    var players=["Martin", "Thomas","Peter","Mathias","Niki"];
+
+    var player_name =players[2];
+
+    // var n = str.indexOf("welcome");
+    document.getElementById("new_text1").innerHTML ="third player name" ;
+    document.getElementById("new_text2").innerHTML =player_name;
+
+    
+
+  }
